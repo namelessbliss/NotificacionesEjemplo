8452e3375e5dce9281f9d6299f4994e7222d18d6
namelessbliss <anonimo2911.2012@gmail.com>
2019-08-02 18:56:03 -0500

    NotificationHandler, notificaciones en Oreo y versiones anteriores

---
+++ app/src/main/java/com/app/nb/notificacionesejemplo/MainActivity.java
@@ -0,0 +1,65 @@
+package com.app.nb.notificacionesejemplo;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextTitle)
+    EditText editTextTitle;
+    @BindView(R.id.editTextMessage)
+    EditText editTextMessage;
+    @BindView(R.id.switchImportance)
+    Switch switchImportance;
+    @BindString(R.string.switch_notifications_on)
+    String switchTextOn;
+    @BindString(R.string.switch_notifications_off)
+    String switchTextOff;
+
+    private boolean isHighImportance = false;
+
+    private NotificationHandler notificationHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        notificationHandler = new NotificationHandler(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick() {
+        sendNotification();
+    }
+
+    @OnCheckedChanged(R.id.switchImportance)
+    public void change(CompoundButton button, boolean isChecked) {
+        isHighImportance = isChecked;
+        // Operador ternario para cambiar el texto del switch
+        switchImportance.setText((isChecked) ? switchTextOn : switchTextOff);
+    }
+
+    private void sendNotification() {
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+
+        if (!TextUtils.isEmpty(title) && !TextUtils.isEmpty(message)) {
+            Notification.Builder builder = notificationHandler.createNotification(title, message, isHighImportance);
+            notificationHandler.getManager().notify(1, builder.build());
+        }
+    }
+
+}
---
+++ app/src/main/java/com/app/nb/notificacionesejemplo/NotificationHandler.java
@@ -0,0 +1,94 @@
+package com.app.nb.notificacionesejemplo;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+public class NotificationHandler extends ContextWrapper {
+
+    private NotificationManager manager;
+
+    public static final String CHANNEL_HIGH_ID = "1";
+    private static final String CHANNEL_HIGH_NAME = "HIGH CHANNEL";
+
+    public static final String CHANNEL_LOW_ID = "2";
+    private static final String CHANNEL_LOW_NAME = "LOW CHANNEL";
+
+    public NotificationHandler(Context base) {
+        super(base);
+        createChannel();
+    }
+
+    public NotificationManager getManager() {
+        if (manager == null) {
+            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        return manager;
+    }
+
+    private void createChannel() {
+        if (Build.VERSION.SDK_INT >= 26) { // Compatible con los canales y notificaciones de android
+            //Creando el HIGH CHANNEL
+            NotificationChannel highChannel = new NotificationChannel(CHANNEL_HIGH_ID, CHANNEL_HIGH_NAME, NotificationManager.IMPORTANCE_HIGH);
+
+            // Configuracion extra
+            highChannel.enableLights(true);// activa leds parpadeantes
+            highChannel.setLightColor(Color.YELLOW);
+            highChannel.setShowBadge(true);
+            highChannel.enableVibration(true);
+            //highChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            highChannel.setSound(defaultSoundUri, null);
+
+            highChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+
+            NotificationChannel lowChannel = new NotificationChannel(CHANNEL_LOW_ID, CHANNEL_LOW_NAME, NotificationManager.IMPORTANCE_LOW);
+
+            lowChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+
+            //Crea los canales
+            getManager().createNotificationChannel(highChannel);
+            getManager().createNotificationChannel(lowChannel);
+        }
+    }
+
+    public Notification.Builder createNotification(String title, String message, boolean isHighImportance) {
+        if (Build.VERSION.SDK_INT >= 26) {
+            if (isHighImportance) {
+                return createNotificationWithChannel(title, message, CHANNEL_HIGH_ID);
+            }
+            return createNotificationWithChannel(title, message, CHANNEL_LOW_ID);
+        }
+        return createNotificationWithoutChannel(title, message);
+    }
+
+    private Notification.Builder createNotificationWithChannel(String title, String message, String channelId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return new Notification.Builder(getApplicationContext(), channelId)
+                    .setContentTitle(title)
+                    .setColor(getResources().getColor(R.color.colorPrimary))
+                    .setContentText(message)
+                    .setSmallIcon(android.R.drawable.stat_notify_chat)
+                    .setAutoCancel(true);
+        }
+        return null;
+    }
+
+    private Notification.Builder createNotificationWithoutChannel(String title, String message) {
+        return new Notification.Builder(getApplicationContext())
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(Notification.DEFAULT_ALL);
+    }
+}
